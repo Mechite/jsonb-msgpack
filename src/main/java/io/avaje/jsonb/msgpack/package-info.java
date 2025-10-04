da94916e83f97074a9857dfe0335e9e48d10ed20
Mechite <contact@mechite.com>
2025-10-04 06:47:59 +0100

    MOVED
    Signed-off-by: Mechite <contact@mechite.com>

--- src/main/java/io/avaje/jsonb/msgpack/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Avaje Jsonb adapter that adds support for the MessagePack binary format.
- *
- * @see io.avaje.jsonb.msgpack.MsgpackAdapter
- * @since 1.11
- */
-package io.avaje.jsonb.msgpack;
